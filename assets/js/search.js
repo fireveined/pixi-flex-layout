426c04675558dace24b10ec22bfb13bf9ea7b3c7
mateuszszymik <mateusz.szymik@beefee.co.uk>
2020-03-30 17:59:02 +0200

    doc added

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"initializeYogaLayout","url":"globals.html#initializeyogalayout","classes":"tsd-kind-function"}]};
